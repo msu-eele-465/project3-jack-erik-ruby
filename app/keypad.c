628f445b9796e3601d91674fd734447b18185969
Ruby Martin <ruby@wolfssl.com>
2025-02-14 21:37:52 -0700

    add functions to keypad.c, not tested

--- app/keypad.c
+++ app/keypad.c
@@ -2,26 +2,60 @@
 #include "../src/keypad.h"
 #include "msp430fr2355.h"
 
+// reversed to match datasheet to match pin indices
 char keyChars[4][4] = {
-    {'1', '2', '3', 'A'},
-    {'4', '5', '6', 'B'},
-    {'7', '8', '9', 'C'},
-    {'*', '0', '#', 'D'}
+    {'D', '#', '0', '*'},  
+    {'C', '9', '8', '7'},  
+    {'B', '6', '5', '4'},  
+    {'A', '3', '2', '1'}  
 };
 
 void init_keypad(Keypad *keypad) {
-    // set keypad row GPIO pins
-    for (int i = 0; i < 4; i++){
-        P6DIR &= ~(1 << keypad->rowPins[i]);        // set pin to input
-        P6REN |= (1 << keypad->rowPins[i]);         // enable pull up/down resistor
-        P6OUT |= (1 << keypad->rowPins[i]);
-        
+    // set keypad row pins as input
+    int i;
+    for (i = 0; i < 4; i++){
+        P6DIR &= ~(keypad->rowPins[i]);        // set pin to input
+        P6REN |= keypad->rowPins[i];         // enable pull up/down resistor
+        P6OUT |= keypad->rowPins[i];         // set pull up resistor
     }
 
-}
-void setLock(Keypad *keypad) {
+    // set keypad col pins as output
+    for (i = 0; i < 4; i++){
+        P2DIR |= keypad->colPins[i];         // set pin to output
+        P2OUT |= keypad->colPins[i];         // set high
+    }
 
-}
-void scanKeypad(Keypad *keypad) {
+    // set passkey
+    for (i = 0; i < 4; i++){
+        keypad->password[i] = passkey[i];
+    }
 
+    // lock keypad
+    keypad->lockState = LOCKED;
+}
+void setLock(Keypad *keypad, int lock) {
+    if (lock != LOCKED)
+        keypad->lockState = UNLOCKED;
+    else
+        keypad->lockState = LOCKED;
+}
+int scanKeypad(Keypad *keypad, char *keyPress) {
+    // for each col, check if there is a LOW row
+    for(int col = 0; col < 4; col++){
+        // clear, set col LOW
+        P2OUT &= ~keypad->colPins[col];
+
+        for(int row = 0; row < 4; row++){
+            if (!(P2IN & keypad->rowPins[row])){
+                keyPress = keyChars[row][col];
+
+                // set col HIGH
+                P2OUT |= keypad->colPins[col];
+                return SUCCESS;
+            }
+        }
+        // set col HIGH
+        P2OUT |= keypad->colPins[col];
+    }
+    return FAILURE;
 }
--- src/keypad.h
+++ src/keypad.h
@@ -1,25 +1,33 @@
 #ifndef  KEYPAD_H
 #define KEYPAD_H
     #include "msp430fr2355.h"
-#include <stdio.h>
+    #include <stdio.h>
     #include </msp430.h>
 
-    #ifndef DMN
-    #define DMN 4
+    #ifndef LOCKED
+    #define LOCKED 1
     #endif
-
+    #ifndef UNLOCKED
+    #define UNLOCKED 0
+    #endif
+    #ifndef SUCCESS
+    #define SUCCESS 1
+    #endif
+    #ifndef FAILURE
+    #define FAILURE 0
+    #endif
 
     typedef struct {
         int lockState;
-        int rowPins[4] = {BIT3, BIT2, BIT1, BIT0};      // order is 5, 6, 7, 8
-        int colPins[4] = {BIT4, BIT5, BIT2, BIT0};      // order is 1, 2, 3, 4
-        int password;
+        int rowPins[4];      // order is 5, 6, 7, 8
+        int colPins[4];      // order is 1, 2, 3, 4
+        int passkey[4];
     } Keypad;
 
-    extern char keyChar [DMN][DMN];
+    extern char keyChar [4][4];
 
     void init_keypad(Keypad *keypad);
-    void setLock(Keypad *keypad);
+    void setLock(Keypad *keypad, int lock);
     void scanKeypad(Keypad *keypad);
 
 #endif
